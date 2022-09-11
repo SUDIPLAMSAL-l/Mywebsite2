b37a4106ab8d4291e8fe4196b60e457cbb6a0d40
Sudip Lamsal <lamsal.sudip99@gmail.com>
2022-09-12 00:40:32 +0900

    website add

---
+++ src/component/todoreact/todo.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./style.css";
+
+const Todo = () => {
+  return (
+    <>
+        <div className="main-div">
+            <div className="child-div">
+                <figure>
+                    <img src="./images/todo.svg" alt="todologo" />
+                    <figcaption>Add Your List Here　✌</figcaption>
+                </figure>
+                <div className="addItems">
+                    <input type="text" 
+                    placeholder = "✍　Add Item"
+                    className = "form-control"
+                    />
+                    <i className="fa fa-plus add-btn"></i>
+                </div>
+                {/* show our Items */}
+                    <div className="showItems">
+                    <button className="btn effect04" data-sm-link-text="Remove All">
+                        <span>CHECK LIST</span>
+                    </button>
+                    </div>        
+            </div>
+        </div>
+    </>
+  );
+};
+
+export default Todo
